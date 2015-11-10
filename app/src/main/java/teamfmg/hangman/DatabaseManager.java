b073b597b11856030deea1a3fd6f79d0fe8b26dc
Ludwig Kratzl <kratzl.ludwig@gmail.com>
2015-11-10 14:47:58 +0100

    Renamed package

---
+++ app/src/main/java/teamfmg/hangman/DatabaseManager.java
@@ -0,0 +1,160 @@
+package teamfmg.hangman;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.CursorIndexOutOfBoundsException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Handles the Database connection with SQLite.<br />
+ * Created by Ludwig 09.11.2015.
+ * @since 0.1
+ */
+public class DatabaseManager extends SQLiteOpenHelper
+{
+
+    private static final int DATABASE_VERSION       = 5;
+    private static final String DATABASE_NAME       = "database.db";
+    private static final String TABLE_USERS_NAME    = "users";
+
+    /**
+     * Creates a new instance of the database handler.
+     * @param context Context class.
+     * @since 0.1
+     */
+    public DatabaseManager(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        //Create user table
+        String createTableStatement =
+                "CREATE TABLE " + TABLE_USERS_NAME + " ( _id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        "username VARCHAR(20) NOT NULL, " +
+                        "password     VARCHAR(30) NOT NULL, " +
+                        "mail VARCHAR(20) NOT NULL);";
+        db.execSQL(createTableStatement);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_USERS_NAME);
+        this.onCreate(db);
+    }
+
+    @Deprecated
+    /**
+     * Deletes the table user
+     * @since 0.1
+     */
+    public void deleteAll()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String statement = "DROP TABLE IF EXISTS" + TABLE_USERS_NAME+";";
+        db.execSQL(statement);
+
+    }
+
+    /**
+     * Adds a user to the database.
+     * @param u User to add
+     * @throws SQLiteException
+     * @since 0.1
+     */
+    public void addUser (User u) throws SQLiteException
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues val = new ContentValues();
+        val.put("username",u.getName());
+        val.put("password", u.getPassword());
+        val.put("mail",u.getMail());
+
+
+        db.insert(TABLE_USERS_NAME, null, val);
+
+        db.close();
+    }
+
+    /**
+     * Gets all registered users
+     * @return List of users.
+     * @since 0.1
+     * @deprecated
+     */
+    public List<User> getUsers()
+    {
+        List<User> users = new LinkedList<>();
+
+        String query = "SELECT  * FROM " + TABLE_USERS_NAME;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor != null && cursor.moveToFirst())
+        {
+            do
+            {
+                User u = new User(cursor.getString(1),cursor.getString(2),cursor.getString(3));
+                // Add user
+                users.add(u);
+            }
+            while (cursor.moveToNext());
+
+            cursor.close();
+        }
+
+        return users;
+    }
+
+    /**
+     * Gets a user by its name.
+     * @param name Name of the user.
+     * @return A User Object.
+     * @throws NullPointerException Exception, if no user was found.
+     * @since 0.1
+     */
+    public User getUser(String name) throws NullPointerException
+    {
+        String query = "SELECT  * FROM " + TABLE_USERS_NAME +
+                " WHERE username LIKE '" + name + "';";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        if(cursor != null)
+        {
+            try
+            {
+                cursor.moveToFirst();
+                return new User(cursor.getString(1), cursor.getString(2), cursor.getString(3));
+            }
+            catch (CursorIndexOutOfBoundsException ex)
+            {
+                return null;
+            }
+            finally
+            {
+                cursor.close();
+            }
+        }
+        else
+        {
+            throw new NullPointerException();
+        }
+    }
+
+    @Override
+    public void close()
+    {
+        //this.getWritableDatabase().close();
+    }
+}
---
+++ app/src/main/java/teamfmg/hangman/User.java
@@ -0,0 +1,80 @@
+package teamfmg.hangman;
+
+/**
+ *  User Object.
+ *  Created by Ludwig 09.11.2015.
+ */
+public class User
+{
+    /**
+     * Creates a new user to be added at the database.
+     * @param name Name
+     * @param password Password
+     * @param mail e mail
+     * @since 0.1
+     */
+    public User(String name, String password, String mail)
+    {
+        this.setName(name);
+        this.setPassword(password);
+        this.setMail(mail);
+    }
+
+    /**
+     * Variables (Strings).
+     */
+    private String name, password, mail;
+
+    /**
+     * Gets the username.
+     * @return String
+     * @since 0.1
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the username
+     * @param name String.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the password.
+     * @return String
+     * @since 0.1
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets the password.
+     * @param password String
+     * @since 0.1
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Gets the E-Mail address.
+     * @return String.
+     * @since 0.1
+     */
+    public String getMail() {
+        return mail;
+    }
+
+    /**
+     * Sets the E-Mail address.
+     * @param mail string
+     * @since 0.1
+     */
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
