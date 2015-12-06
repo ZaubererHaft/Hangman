2c71314e46ed4d725b4f242b17ca554a81226d14
Ludwig Kratzl <kratzl.ludwig@gmail.com>
2015-12-06 19:56:25 +0100

    Bug fix

--- app/src/main/java/teamfmg/hangman/DatabaseManager.java
+++ app/src/main/java/teamfmg/hangman/DatabaseManager.java
@@ -27,7 +27,7 @@ public class DatabaseManager extends SQLiteOpenHelper
     /**
      * Version of the database.
      */
-    private static final int DATABASE_VERSION       = 68;
+    private static final int DATABASE_VERSION       = 70;
     /**
      * Name of the database
      */
